6344692acf6b629ea0478f3a85e93315f33cdb12
Jeff Cook <jecook2692@gmail.com>
2020-04-01 15:46:52 -0400

    adds week file

--- exercise4.py
+++ exercise4.py
@@ -1,26 +1,28 @@
 day = int(input('Day(0-6)? '))
 day = int(day)
 
-if day ==- 0:
+if day == 0:
     print ("It's Sunday")
 
 
-elif day ==- 1:
+elif day == 1:
     print ("It's Monday")
 
-elif day ==- 2:
+elif day == 2:
     print ("It's Tuesday")   
 
-elif day ==- 3:
+elif day == 3:
     print ("It's Wednesday")
 
-elif day ==- 4:
+elif day == 4:
     print ("It's Thursday")
 
-elif day ==- 5:
+elif day == 5:
     print ("It's Friday")
 
-else day ==- 6:
+elif day == 6:
     print ("It's Saturday")
 
 
+else:
+    print (f'Enter a valid number!')
