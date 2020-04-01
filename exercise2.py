5612a78269ca5fd75d485e49b480c90d5800471a
Jeff Cook <jecook2692@gmail.com>
2020-04-01 13:20:58 -0400

    adds upper file

---
+++ exercise2.py
@@ -0,0 +1,5 @@
+name = input('What is your name? ' .upper())
+greeting = f'Hello, {name} '
+print (greeting .upper())
+length_of_name = f'Your name has {len(name)} letters! Awesome'
+print (length_of_name .upper())
